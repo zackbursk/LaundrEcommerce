0bfde03beb6cfeb97a6b2c85a7bce3c17cbb0b58
Alex Good <gooda1@ufl.edu>
2020-11-26 20:24:08 -0500

    Added tessellation and gave cart sidebar higher z-index

--- laundr/src/data.js
+++ laundr/src/data.js
@@ -4,7 +4,7 @@ export default {
             name: 'Fresh Air',
             _id: 'Fresh Air',
             image: '/img/products/fresh.png',
-            tessellation: '/img/tessellation/fresh_pattern2.png',
+            tessellation: '/img/tessellation/fresh_pattern.png',
             price: 18.99,
             color: '#8dcfdd'
         },
@@ -12,7 +12,7 @@ export default {
             name: 'Coffee Vanilla',
             _id: 'Coffee Vanilla',
             image: '/img/products/coffee.png',
-            tessellation: '/img/tessellation/vanilla_pattern2.png',
+            tessellation: '/img/tessellation/vanilla_pattern.png',
             price: 18.99,
             color: '#e2cea3'
         },
@@ -20,7 +20,7 @@ export default {
             name: 'Eucalyptus Tea Tree',
             _id: 'Eucalyptus Tea Tree',
             image: '/img/products/eucalyptus.png',
-            tessellation: '/img/tessellation/eucalyptus_pattern2.png',
+            tessellation: '/img/tessellation/eucalyptus_pattern.png',
             price: 18.99,
             color: '#48c47e'
         },
@@ -28,7 +28,7 @@ export default {
             name: 'White Gardenia',
             _id: 'White Gardenia',
             image: '/img/products/gardenia.png',
-            tessellation: '/img/tessellation/gardenia_pattern2.png',
+            tessellation: '/img/tessellation/gardenia_pattern.png',
             price: 18.99,
             color: '#f4ea9c'
         },
@@ -36,7 +36,7 @@ export default {
             name: 'Mahogany Teakwood',
             _id: 'Mahogany Teakwood',
             image: '/img/products/mahogany.png',
-            tessellation: '/img/tessellation/mahogany_pattern2.png',
+            tessellation: '/img/tessellation/mahogany_pattern.png',
             price: 18.99,
             color: '#5b5b5b'
         },
@@ -44,7 +44,7 @@ export default {
             name: 'Watermelon Cucumber',
             _id: 'Watermelon Cucumber',
             image: '/img/products/watermelon.png',
-            tessellation: '/img/tessellation/watermelon_pattern2.png',
+            tessellation: '/img/tessellation/watermelon_pattern.png',
             price: 18.99,
             color: '#ff5656'
         },
