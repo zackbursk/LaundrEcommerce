cab32d1caf3c15207809b7125149b38bd136cc82
Alex Good <gooda1@ufl.edu>
2020-12-04 00:01:23 -0500

    Added video header, footer, product description, and changed html head

--- laundr/src/components/Home.js
+++ laundr/src/components/Home.js
@@ -12,6 +12,12 @@ export default class home extends Component{
             <div className="grid-container">
             <main className="main">
             <div className="content">
+                <div className="header-video">
+                    <div className="header-video-text">Delightfully Fresh Laundry Detergent</div>
+                    <div className="header-video-container">
+                        <video autoPlay loop muted src="./video/oceanSample.mp4"></video>
+                    </div>
+                </div>
                 <ul className="products">
                     {
                         this.props.products.map(product =>
@@ -22,6 +28,7 @@ export default class home extends Component{
                                         <a href="product.html">{product.name}</a>
                                     </div>
                                     <div className="product-price">${product.price}</div>
+                                    <div className="product-description">{product.description}</div>
                                     <button
                                         onClick={() => this.props.addToCart(product)}
                                         className="button primary"
@@ -34,7 +41,23 @@ export default class home extends Component{
                     }
                 </ul>
                 <footer className="footer">
-                    All Rights Reserved
+                    <div className="footer-contact">
+                        Contact Info:
+                        <div className="footer-info">Phone: (352) 363 - 5211</div>
+                        <div className="footer-info">Email: support@laundr.io</div>
+                        <div className="footer-info">Website: https://laundr.io</div>
+                        <br/>
+                        Social Media:
+                        <div className="footer-info"><a href="http://facebook.com/laundrofficial" target="_blank">Facebook</a></div>
+                        <div className="footer-info"><a href="http://instagram.com/laundrofficial" target="_blank">Instagram</a></div>
+                        <div className="footer-info"><a href="https://twitter.com/laundrofficial" target="_blank">Twitter</a></div>
+                    </div>
+                    <div className="footer-news">
+                            Latest News
+                            <br/>
+                            <div className="footer-news-info">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+                        </div>
+                    <div className="footer-bottom-text">All Rights Reserved | Privacy Policy | Terms of Service</div>
                 </footer>
             </div>
 
--- laundr/src/data.js
+++ laundr/src/data.js
@@ -6,7 +6,8 @@ export default {
             image: '/img/products/fresh.png',
             tessellation: '/img/tessellation/fresh_pattern.png',
             price: 18.99,
-            color: '#8dcfdd'
+            color: '#8dcfdd',
+            description: "A soft, cottony fragrance that smells even cleaner than clean. Great for leaping face-first into a gigantic, fluffy comforter."
         },
         {
             name: 'Coffee Vanilla',
@@ -14,7 +15,8 @@ export default {
             image: '/img/products/coffee.png',
             tessellation: '/img/tessellation/vanilla_pattern.png',
             price: 18.99,
-            color: '#e2cea3'
+            color: '#e2cea3',
+            description: "Smells like the steam rising off of a hot vanilla latte. Great for sitting in an armchair in a quaint neighborhood cafe."
         },
         {
             name: 'Eucalyptus Tea Tree',
@@ -22,7 +24,8 @@ export default {
             image: '/img/products/eucalyptus.png',
             tessellation: '/img/tessellation/eucalyptus_pattern.png',
             price: 18.99,
-            color: '#48c47e'
+            color: '#48c47e',
+            description: "A natural scent inspired by popular aromatherapy oil mixtures. Great for towels, bath robes, and having a “me day.”"
         },
         {
             name: 'White Gardenia',
@@ -30,7 +33,8 @@ export default {
             image: '/img/products/gardenia.png',
             tessellation: '/img/tessellation/gardenia_pattern.png',
             price: 18.99,
-            color: '#f4ea9c'
+            color: '#f4ea9c',
+            description: "A sweet and floral perfume with relaxing notes, best used after a carefree frolic through a field of wildflowers."
         },
         {
             name: 'Mahogany Teakwood',
@@ -38,7 +42,8 @@ export default {
             image: '/img/products/mahogany.png',
             tessellation: '/img/tessellation/mahogany_pattern.png',
             price: 18.99,
-            color: '#5b5b5b'
+            color: '#5b5b5b',
+            description: "A great everyday alternative to cologne, or for a getaway to a log cabin hidden deep in a snowy forest."
         },
         {
             name: 'Watermelon Cucumber',
@@ -46,7 +51,8 @@ export default {
             image: '/img/products/watermelon.png',
             tessellation: '/img/tessellation/watermelon_pattern.png',
             price: 18.99,
-            color: '#ff5656'
+            color: '#ff5656',
+            description: "A sweet, cool and refreshing smell for brightening up your day. Great for picnic blankets, overalls, and being in a generally good mood."
         },
     ]
 }
